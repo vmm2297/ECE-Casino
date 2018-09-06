c2a708e6363be17e5bfe710950ecd9c256b622b5
vmm2297 <vmiranda022097@gmail.com>
2018-09-06 15:14:08 -0700

    blackjack: added shuffle cards function

--- BLACKJACK.py
+++ BLACKJACK.py
@@ -1,17 +1,28 @@
 #Victor's Casino (for now, we just have Blackjack)
+import random
 
 def casino_entrance():
-	print("Hi! Welcome to Victor's Casino.")
+	#player inputs name and chooses game
+	print("Welcome to the ECE Casino!")
 	print("What is you name?")
 	user_name = raw_input(">")
 	print("Hi %s, what game do you want to play today?")%user_name.capitalize()
 	user_game = raw_input(">")
 	if (user_game.lower() in ["blackjack","black jack","21"]):
-		print("Okay! One game of BLACKJACK coming right up...")
+		print("Gotcha! One game of BLACKJACK coming right up...")
 	else:
-		print("Sorry! We do not have that game yet. Come back another day.")
+		print("Sorry! We do not have that game yet. Please come back another day.")
 
 def shuffle_deck():
-	print("Deck is shuffled!")
+	#fresh deck of cards is shuffled
+	card_values = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King']
+	card_types = ['Spades','Diamonds','Hearts','Clubs']
+	deck = []
+	for i in card_types:
+		for j in card_values:
+			deck.append(j+' of '+i)
+	random.shuffle(deck)
+	print("A new deck of cards has been shuffled.")
+	return deck
 
-casino_entrance()
+shuffle_deck()
