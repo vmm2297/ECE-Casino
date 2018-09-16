f6f0a679e8d5c4e7d43e2db7730aec5fca7f754e
vmm2297 <vmiranda022097@gmail.com>
2018-09-15 21:19:05 -0700

    blackjack: small comment changes and bug fixes

--- BLACKJACK.py
+++ BLACKJACK.py
@@ -1,4 +1,11 @@
-#Victor's Casino (for now, we just have Blackjack)
+#ECE Casino
+#Hosted by ECE Undergradute Student Council at UCSD
+
+#Current Games:
+#	- BLACKJACK (Victor)
+#	- WAR (Chau)
+
+#Games are played in the terminal console.
 import random
 
 def enter_casino():
@@ -27,12 +34,12 @@ def play_blackjack():
 
 	#hands are shown, sums are computed
 	print("Your current hand is %s.") % str(player_hand)
-	player_sum = get_hand_sum(player_hand)
+	player_sum = get_hand_sum(player_hand,False)
 	print("Your current sum is %s.") % str(player_sum)
 	print("The dealer's first card is the %s with the second card face down.") % str(dealer_hand[0])
-	dealer_sum = get_hand_sum(dealer_hand)
+	dealer_sum = get_hand_sum(dealer_hand,True)
 	
-	#checking "natural blackjack"
+	#checking "natural blackjack" (when either player gets 21 on first hand)
 	if(player_sum==21 and dealer_sum!=21):
 		print("BLACKJACK! You automatically beat the dealer who has a hand of %s.") % str(dealer_hand)
 		print("Congratulations!")
@@ -46,15 +53,15 @@ def play_blackjack():
 	#player choice to HIT or STAND
 	else:
 		print("Do you wish to HIT or STAND?")
-		player_choice = ""
+		player_choice = raw_input(">")
 		while(player_choice.lower()!="hit" and player_choice.lower()!="stand"):
+			print("That is not a valid option! Please choose either HIT or STAND.")
 			player_choice = raw_input(">")
-			if(player_choice.lower()!="hit" and player_choice.lower()!="stand"):
-				print("That is not a valid option! Please choose either HIT or STAND.")
-				player_choice = raw_input(">")
-		print(player_choice)
+		print("You have chosen to %s.") % player_choice.upper()
 	#player HIT
 	#player STAND
+	#dealer HIT
+	#dealer STAND
 
 def shuffle_deck():
 	#returns a freshly shuffled deck of cards (list)
@@ -68,12 +75,12 @@ def shuffle_deck():
 	print("A new deck of cards has been shuffled.")
 	return deck
 
-def get_card_value(card,dealer=False):
+def get_card_value(card,dealer):
 	#returns the value of the drawn card (int)
 	card = card.split(' ')[0]
 	if card in ('Jack','Queen','King'):
 		return int(10)
-	elif(card=='Ace' and not dealer):
+	elif(card=='Ace' and (not dealer)):
 		print("You drew an ACE! Do you want the value to be 1 or 11?")
 		choice = raw_input(">")
 		while(True):
@@ -86,14 +93,15 @@ def get_card_value(card,dealer=False):
 				choice = raw_input(">")
 	elif(card=='Ace' and dealer):
 		#better logic for choosing dealer ACE value!!!
-		return int(1)
+		return int(11)
 	else:
 		return int(card)
 
-def get_hand_sum(hand):
+def get_hand_sum(hand,dealer):
+	#returns the value of the hand (int)
 	hand_sum = 0
 	for i in hand:
-		hand_sum = hand_sum + get_card_value(i)
+		hand_sum = hand_sum + get_card_value(i,dealer)
 	return hand_sum
 
 #player enters the casino
