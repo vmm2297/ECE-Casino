0125e0321ff0bb63aa1e930da22d5745adef6e14
Alex Garcia <alexsebastian.garcia@gmail.com>
2018-09-19 20:07:57 -0700

    format code differently, add some more messages, clean up some ui

--- CASINO.py
+++ CASINO.py
@@ -8,16 +8,63 @@
 #	- WAR (Chau)
 
 #Player enters casino, inputs name, and chooses game.
-current_games = "BLACKJACK and WAR (in progress)"
-print("Welcome to the ECE Casino!")
-print("What is you name?")
-user_name = raw_input(">")
-print("Hi %s, what game do you want to play today?")%user_name.capitalize()
-print("Our current selection of games include %s.")%current_games
-user_game = raw_input(">")
-if user_game.lower() in ("blackjack","black jack","21"):
-	print("Gotcha! One game of BLACKJACK coming right up...")
-	import BLACKJACK
-	print(BLACKJACK.play_blackjack())
-else:
-	print("Sorry! We do not have that game yet. Please come back another day.")
+
+CASINO_TITLE = """
+______ _____ ______    _____           _____ _____ _   _  ____  
+|  ____/ ____|  ____|  / ____|   /\    / ____|_   _| \ | |/ __ \ 
+| |__ | |    | |__    | |       /  \  | (___   | | |  \| | |  | |
+|  __|| |    |  __|   | |      / /\ \  \___ \  | | | . ` | |  | |
+| |___| |____| |____  | |____ / ____ \ ____) |_| |_| |\  | |__| |
+|______\_____|______|  \_____/_/    \_\_____/|_____|_| \_|\____/ 
+
+"""
+
+BYE_TITLE = """
+______     ________              ______     ________ 
+|  _ \ \   / /  ____|            |  _ \ \   / /  ____|
+| |_) \ \_/ /| |__      ______   | |_) \ \_/ /| |__   
+|  _ < \   / |  __|    |______|  |  _ < \   / |  __|  
+| |_) | | |  | |____             | |_) | | |  | |____ 
+|____/  |_|  |______|            |____/  |_|  |______|
+
+"""
+current_games = ["BLACKJACK", "WAR (in progress)"]
+
+def main():
+
+    print(CASINO_TITLE)
+
+    print("Welcome to the ECE Casino!")
+
+    print("What is you name?")
+
+    # Keep going until we get their name
+    got_name = False
+    while not got_name:
+            try:
+                    user_name = raw_input("> ")
+                    if len(user_name) == 0:
+                            throw Exception()
+            except:
+                    print("Please provide your name!")
+
+    print("Hi %s, what game do you want to play today?" % user_name.capitalize())
+    print("Our current selection of games include:")
+    
+    # +1 here and the -1 below to display "1,2,3" instead of "0,1,2"
+    for i, game in enumerate(current_games):
+            print("[%d] %s" % (i+1, game))
+
+    user_game = int(raw_input("> ")) - 1
+
+    if user_game == 0:
+            print("Gotcha! One game of BLACKJACK coming right up...")
+            import BLACKJACK
+            print(BLACKJACK.play_blackjack())
+    else:
+            print("Sorry! We do not have that game yet. Please come back another day.")
+    
+    print(BYE_TITLE)
+
+if __name__ == "__main__":
+    main()
